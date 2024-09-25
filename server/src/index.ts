492664210d1624303b143d08524c1cb051e57c4b
Adham Nasser <adhamxiii22@gmail.com>
2024-09-25 12:51:32 +0300

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,9 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-
+app.use('/', () => {
+    console.log('Hello World');
+})
 
 mongoose
     .connect(`${process.env.MONGO_URI}`)
